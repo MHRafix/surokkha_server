b35f0e8b44de8ce2992f0ef36aceb7d71d1528eb
MHRafix <fmhrafiz27@gmail.com>
2023-05-13 12:25:50 +0600

    fix err

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `<div><h1>Hello this is SUROKKHA SERVER ⛎⛎⛎!</h1> 
-    <a href="/vaccine-certificate/create">Create Vaccine certificate</a> <br />
-    <a href="/vaccine-certificate/certificates">Get all Vaccine certificate</a> <br />
-    <a href="/vaccine-certificate/singleCertificate/[id]">Single Vaccine certificate</a> <br />
-    <a href="/vaccine-certificate/updateCertificates">Update Vaccine certificate</a> <br />
-    <a href="/vaccine-certificate/deleteCertificate">Delete Vaccine certificate</a> 
-    </div>`;
+    // return `<div><h1></h1>
+    // <a href="/vaccine-certificate/create">Create Vaccine certificate</a> <br />
+    // <a href="/vaccine-certificate/certificates">Get all Vaccine certificate</a> <br />
+    // <a href="/vaccine-certificate/singleCertificate/[id]">Single Vaccine certificate</a> <br />
+    // <a href="/vaccine-certificate/updateCertificates">Update Vaccine certificate</a> <br />
+    // <a href="/vaccine-certificate/deleteCertificate">Delete Vaccine certificate</a>
+    // </div>`;
+    return 'Hello this is SUROKKHA SERVER ⛎⛎⛎!';
   }
 }
